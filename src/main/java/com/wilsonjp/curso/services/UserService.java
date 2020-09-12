45d509901e273268f6850984b6119c1fdfe29871
Wilson Pereira <wilsonjp.dev@outlook.com>
2020-09-12 19:14:04 -0300

    Exception handling - delete

--- src/main/java/com/wilsonjp/curso/services/UserService.java
+++ src/main/java/com/wilsonjp/curso/services/UserService.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.wilsonjp.curso.entities.User;
 import com.wilsonjp.curso.repositories.UserRepository;
+import com.wilsonjp.curso.services.exceptions.DatabaseException;
+import com.wilsonjp.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +25,7 @@ public class UserService {
 	
 	public User infById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() ->new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
